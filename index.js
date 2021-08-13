cd2a20c01bb65533429a27cdce7ee801ef202514
Vlad Frangu <kingdgrizzle@gmail.com>
2021-08-13 14:39:38 +0200

    fix: disable the rule in JS and in TS Overrides take precedence..

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ module.exports = {
                 "react/prop-types": "off",
                 // note you must disable the base rule as it can report incorrect errors
                 "no-unused-vars": "off",
+                // TypeScript doesn't support imports with `.ts` extensions, and `.js` makes the rule complain
+                "import/extensions": "off",
                 // This rule extends the base eslint/no-unused-vars rule. It adds support for TypeScript features, such as types.
                 "@typescript-eslint/no-unused-vars": ["error"],
                 // Allow semicolons at the end of the code block
